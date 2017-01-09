06ea1f52996a1c433c2b9f212d6291e4bd19dce8
Your Name <name@example.com>
2017-01-09 10:44:45 +0700

    Read File From txt

--- sudoku.js
+++ sudoku.js
@@ -1,1 +1,6 @@
 "use strict"
+
+var fs = require('fs')
+var sudoku = require('sudoku-js');
+
+var board_string = fs.readFileSync('set-04_peter-norvig_11-hardest-puzzles.txt').toString().split("\n")[0].trim()
